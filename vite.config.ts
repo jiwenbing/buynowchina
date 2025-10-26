c5f4d75578d33c42f21e8d93011a73417ea41a14
籍文兵（Wenbing Ji） <jiwenbing@shuidihuzhu.com>
2025-10-26 12:04:48 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 使用相对路径
+  base: 'https://www.xfx365.com/buynowchina/dist/', // 使用相对路径
   server: {
     allowedHosts: [
       'www.letsfund.us',
